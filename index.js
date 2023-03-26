232aa50d4b5ee893096f4c07c3773b6413179ba2
ajha417 <ajha417@rku.ac.in>
2023-03-26 16:49:31 +0530

    setup database in index file

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 const routes = require('./src/routes/crmRoutes')
 const PORT = 3000;
 
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+// connecting mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/Building_Api',{
+    useNewUrlParser:true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
 routes(app)
 app.get('/',(req,res)=>{
     res.send("hello world")

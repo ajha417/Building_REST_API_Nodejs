9f82116e0ff6beace1e7f1517a342ac17e8930fa
ajha417 <ajha417@rku.ac.in>
2023-03-26 15:26:41 +0530

    integrating middleware in routes

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,16 +1,38 @@
 const routes = (app)=>{
     app.route('/contact')
-    .get((req,res)=>
-    res.send("this is get route")
-    ).post((req,res)=>
+    .get((req,res,next)=>
+    {
+        console.log(`the original request point is ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+    },(req,res,next)=>{
+
+        res.send("this is get route");
+        next();
+    }
+    ).post((req,res,next)=>{
+        console.log(`the original request point is ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+    },
+    (req,res,next)=>{
         res.send("this is post route")
+        next();
+    }
     )
 
     app.route('/contact/:contactId')
-    .put((req,res)=>
+    .put((req,res,next)=>{
+        console.log(`the original request point is ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+    },(req,res,next)=>{
+        
         res.send('This is update request')
+        next();
+    }
     )
-    .delete((req,res)=>
+    .delete((req,res,next)=>
         res.send('This is delete request')
     )
 }
